0f38dfc213f5056f7e2c09d6357affd2ae61db89
Nuno Cardoso <up201706162@fe.up.pt>
2021-04-11 17:35:03 +0100

    Refactored code

--- lineDetection.py
+++ lineDetection.py
@@ -2,14 +2,27 @@ import cv2
 import numpy as np
 import math
 
+# Auxiliary functions
+def show_image(title, image, scale_percent):
+    imgResized = cv2.resize(image, (int(image.shape[1] * scale_percent / 100), int(image.shape[0] * scale_percent / 100)))
+    cv2.imshow(title, imgResized)
+
 # Opening an image
-img = cv2.imread('./assets/images/b/IMG_0869.jpg', cv2.COLOR_BGR2GRAY)
+imgOriginal = cv2.imread('./assets/images/b/IMG_0869.jpg')
+imgGrey = cv2.cvtColor(imgOriginal, cv2.COLOR_BGR2GRAY)
+
+#------------------TESTING ZONE-------------------------------------
+
+# Gaussian Blur
+img = cv2.GaussianBlur(imgGrey,(7,7),0)
+
+#-------------------------------------------------------
 
 # Using a Canny Filter
-imgWithCanny = cv2.Canny(img, 60, 100, None, 3)
+imgWithCanny = cv2.Canny(imgGrey, 60, 100, None, 3)
 
 # Copy edges to the images that will display the results in BGR
-cdst = cv2.cvtColor(imgWithCanny, cv2.COLOR_GRAY2BGR)
+cdst = imgOriginal.copy()
 
 # Standard Hough Line Transform
 lines = cv2.HoughLinesP(imgWithCanny, 1, np.pi / 180, 50, maxLineGap=50)
@@ -18,16 +31,19 @@ lines = cv2.HoughLinesP(imgWithCanny, 1, np.pi / 180, 50, maxLineGap=50)
 if lines is not None:
     for i in range(0, len(lines)):
         l = lines[i][0]
-        cv2.line(cdst, (l[0], l[1]), (l[2], l[3]), (0,0,255), 3, cv2.LINE_AA)
+        cv2.line(cdst, (l[0], l[1]), (l[2], l[3]), (0,255,0), 3, cv2.LINE_AA)
 
-# Print Original Grey Image
-scale_percent = 50
-imgResized = cv2.resize(img, (int(img.shape[1] * scale_percent / 100), int(img.shape[0] * scale_percent / 100)))
-cv2.imshow('Img', imgResized)
+# Print Original Image
+scale_percent = 20
+#show_image('Img', imgOriginal, scale_percent)
+
+# Print Grey Image
+show_image('Img Grey', imgGrey, scale_percent)
+
+# Print Gaussian Image
+show_image('Img Gaussian', img, scale_percent)
 
 # Print Final Image
-imgResizedCanny = cv2.resize(imgWithCanny, (int(imgWithCanny.shape[1] * scale_percent / 100), int(imgWithCanny.shape[0] * scale_percent / 100)))
-imgResizedCannyLines = cv2.resize(cdst, (int(cdst.shape[1] * scale_percent / 100), int(cdst.shape[0] * scale_percent / 100)))
-cv2.imshow("Img Canny", imgResizedCanny)
-cv2.imshow("Detected Lines (in red) - Standard Hough Line Transform", imgResizedCannyLines)
+#show_image('Img Canny', imgWithCanny, scale_percent)
+show_image('Detected Lines (in red) - Standard Hough Line Transform', cdst, scale_percent)
 cv2.waitKey(0)
